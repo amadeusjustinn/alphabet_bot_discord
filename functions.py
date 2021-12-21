910eb4d50663157056a5ceb6e90991512849a691
Amadeus Justinn Haryanto <amadeusjustinn@gmail.com>
2021-12-21 03:00:45 -0600

    first content

---
+++ functions.py
@@ -0,0 +1,62 @@
+def words_alphabetical(message):
+    '''
+    Checks if the words in the message are arranged in alphabetical order
+
+    Parameters
+    -----
+    - message
+
+        string: Message to be checked
+
+    Returns
+    -----
+    boolean value (whether words in the message are arranged in alphabetical order)
+    '''
+
+    # Split the message into its constituent words and
+    # store them in <words> if their first character is a letter
+    words_etc = message.split()
+    words = [word.lower() for word in words_etc if word[0].isalpha()]
+
+    # If there are fewer than 5 such words, disregard message
+    if len(words) < 5:
+        return False
+
+    # Perform the check
+    return words == sorted(words)
+
+
+def all_letters(message):
+    '''
+    Checks if the message contains every letter in the English alphabet
+
+    Parameters
+    -----
+    - message
+
+        string: Message to be checked
+
+    Returns
+    -----
+    boolean value (whether the message contains every letter in the English alphabet)
+    '''
+
+    # Create a set to store the unique group of letters of the message
+    curr_set = set()
+
+    # Split the message into separate characters and
+    # store them in <all_chars> if they are letters
+    all_chars = [char1.lower() for char1 in message if char1.isalpha()]
+
+    # If there are fewer than 26 letters, disregard message (condition cannot possibly be met)
+    if len(all_chars) < 26:
+        return False
+
+    for char2 in all_chars:
+        curr_set.add(char2)
+
+        # If there are 26 UNIQUE letters, condition has been met
+        if len(curr_set) == 26:
+            return True
+
+    return False
---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+import json
+import functions as func
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in".format(client))
+    return
+
+
+@client.event
+async def on_message(message):
+    with open("data.json") as file1:
+        data = json.load(file1)
+        file1.close()
+
+    data["wa"]["checked_total"] += 1
+    data["al"]["checked_total"] += 1
+
+    if message.author == client.user:
+        return
+
+    if func.words_alphabetical(message.content):
+        data["wa"]["checked_fits"] += 1
+        wact = data["wa"]["checked_total"]
+        wacf = data["wa"]["checked_fits"]
+        await message.channel.send(f"Would you look at that, all of the words in your message are in alphabetical order.\nI now have checked {wact} messages so far, and only {wacf} of them are in alphabetical order.")
+        if func.all_letters(message.content):
+            data["al"]["checked_fits"] += 1
+            alcf = data["al"]["checked_fits"]
+            await message.channel.send(f"I also noticed that your message has all the letters in the alphabet. Only {alcf} of them are like that.")
+
+    elif func.all_letters(message.content):
+        data["al"]["checked_fits"] += 1
+        alct = data["al"]["checked_total"]
+        alcf = data["al"]["checked_fits"]
+        await message.channel.send(f"Would you look at that, your message has all the letters in the alphabet.\nI now have checked {alct} messages so far, and only {alcf} of them have all the letters in the alphabet.")
+
+    with open("data.json") as file2:
+        json.dump(data, file2, "")
+        file2.close()
+
+client.run("OTIxODE3MTM4NTU2MjAzMDg4.Yb4bHA.H8KjQgH19HvtZffQjHaQQqTHY8s")
